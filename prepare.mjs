d2d74e30276b3c365f275f14f67c1d2273cc0cc2
Artem Dzhemesiuk <programming@zziger.me>
2023-06-22 22:26:26 +0200

    Improved prepare script

--- prepare.mjs
+++ prepare.mjs
@@ -1,36 +1,62 @@
 import { exec } from 'child_process';
+import * as fs from 'fs';
+import chalk from 'chalk';
 
 /**
  * Execute simple shell command (async wrapper).
  * @param {String} cmd
  * @param {String?} cwd
- * @return {Object} { stdout: String, stderr: String }
+ * @return {Promise<{ stdout: String, stderr: String } | undefined>}
  */
 async function sh(cmd, cwd) {
     return new Promise(function (resolve, reject) {
-        console.log(`Executing ${cmd} in ${cwd || '.'}`);
-        exec(cmd, { cwd }, (err, stdout, stderr) => {
+        console.log(chalk.gray(`Executing ${chalk.white.bold(cmd)} in ${chalk.white.bold(cwd || '.')}`));
+        const proc = exec(cmd, { cwd }, (err, stdout, stderr) => {
             if (err) {
                 err.cwd = cwd;
-                reject(err);
+                console.error(chalk.red(`=== Failed to execute ${cmd} in ${cwd || '.'}:\n${String(err)}`));
+                resolve(undefined);
             } else {
                 resolve({ stdout, stderr });
             }
         });
+        proc.stdout.pipe(process.stdout);
+        proc.stderr.pipe(process.stderr);
     });
 }
 
+/**
+ * Check if directlory is empty (async wrapper).
+ * @param {String} path
+ * @return {Promise<boolean>}
+ */
+async function isDirEmpty(path) {
+    const files = await fs.promises.readdir(path);
+    return files.length === 0;
+}
+
 async function main() {
-    await sh("git submodule init");
-    await sh("git checkout dev", "./versioned_docs/version-dev");
-    await sh("git checkout rc", "./versioned_docs/version-rc");
-    await sh("git checkout release", "./versioned_docs/version-release");
-    await sh("git checkout master", "./js_versioned_docs/version-dev/repo");
-    await sh("git checkout master", "./js_versioned_docs/version-rc/repo");
-    await sh("git checkout master", "./js_versioned_docs/version-release/repo");
-    await sh("git checkout master", "./jsv2_versioned_docs/version-dev/repo");
-    await sh("git checkout master", "./jsv2_versioned_docs/version-rc/repo");
-    await sh("git checkout master", "./jsv2_versioned_docs/version-release/repo");
+    const shit = await sh("git submodule init");
+
+    const files = {
+        "./versioned_docs/version-dev": "dev",
+        "./versioned_docs/version-rc": "rc",
+        "./versioned_docs/version-release": "release",
+        "./js_versioned_docs/version-dev/repo": "master",
+        "./js_versioned_docs/version-rc/repo": "master",
+        "./js_versioned_docs/version-release/repo": "master",
+        "./jsv2_versioned_docs/version-dev/repo": "master",
+        "./jsv2_versioned_docs/version-rc/repo": "master",
+        "./jsv2_versioned_docs/version-release/repo": "master",
+    }
+
+    for (const [path, branch] of Object.entries(files)) {
+        console.log(chalk.blue('Updating submodule ' + chalk.bold(path)));
+        if (await isDirEmpty(path))
+            await sh("git submodule update " + path);
+        await sh("git checkout " + branch, path);
+        console.log();
+    }
 }
 
 main();

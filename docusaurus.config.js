d04425d3f6595a56a7412ce7a5809d26fa4f4f76
Artem Dzhemesiuk <programming@zziger.me>
2023-07-04 22:37:03 +0200

    Changed baseaddr to the new domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'http://new-docs.alt-mp.com',
+    url: 'https://altv.dev',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
